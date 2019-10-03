82cca728e9f544dc854c80d823ba76c2dd2a1536
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-03 18:00:15 +0330

    Madahi download video quality change

--- macros.cmd
+++ macros.cmd
@@ -3,4 +3,4 @@ ls=dir
 apache_log=tail -f C:\xampp\apache\logs\error.log
 doskey_backup="doskey /macros >C:\Users\javad\macros.cmd"
 ln=mklink
-madahi=youtube-dl -f 22,140 --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c --write-thumbnail -o "C:\Users\javad\Music\My_madahi\%(ext)s\%(title)s.%(ext)s" $*
+madahi=youtube-dl -f best,140 --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c --write-thumbnail -o "C:\Users\javad\Music\My_madahi\%(ext)s\%(title)s.%(ext)s" $*
