5ca12cbba84a27852c0f6ca3c041cbbff319e3e3
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-11-01 10:47:55 +0330

    add cat command

--- macros.cmd
+++ macros.cmd
@@ -1,6 +1,8 @@
 tor=C:\Users\javad\Downloads\Progs\Tor\tor.exe 
 ls=dir
 apache_log=tail -f C:\xampp\apache\logs\error.log
-doskey_backup="doskey /macros >C:\Users\javad\macros.cmd"
+doskey_backup=echo "doskey /macros >C:\Users\javad\macros.cmd"
+cat=type $*
 ln=mklink
 madahi=youtube-dl -f best,140 --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c --write-thumbnail -o "C:\Users\javad\Music\My_madahi\%(ext)s\%(title)s.%(ext)s" $*
+clink="C:\PROGRA~2\clink\043213~1.9/clink_x64.exe" --cfgdir "C:\Users\javad\AppData\Local.\clink" $*
