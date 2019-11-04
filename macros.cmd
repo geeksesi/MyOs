b8e30886bafcce5f3b235b203d6aa2e94b6aa6fd
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-11-04 10:05:12 +0330

    arabic music alias added

--- macros.cmd
+++ macros.cmd
@@ -1,8 +1,9 @@
 tor=C:\Users\javad\Downloads\Progs\Tor\tor.exe 
 ls=dir
 apache_log=tail -f C:\xampp\apache\logs\error.log
-doskey_backup=echo "doskey /macros >C:\Users\javad\macros.cmd"
 cat=type $*
+doskey_backup=echo "doskey /macros >C:\Users\javad\macros.cmd"
 ln=mklink
 madahi=youtube-dl -f best,140 --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c --write-thumbnail -o "C:\Users\javad\Music\My_madahi\%(ext)s\%(title)s.%(ext)s" $*
+arabicmusic=youtube-dl -f 140 --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c --write-thumbnail -o "C:\Users\javad\Music\Arabic\%(title)s.%(ext)s" $*
 clink="C:\PROGRA~2\clink\043213~1.9/clink_x64.exe" --cfgdir "C:\Users\javad\AppData\Local.\clink" $*
