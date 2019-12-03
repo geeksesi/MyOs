55946223a0e676160d9b4ad11621dba13d98949e
geeksesi <geeksesi@gmail.com>
2019-12-03 18:11:56 +0330

    restart my shit emacs config

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -2,25 +2,10 @@
 ;;; .doom.d/packages.el
 
 ;;; Examples:
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-
-
-(package! rust-mode)
-(package! eglot)
-;;(package! lsp-mode)
-(package! cargo)
-(package! rutic)
-(package! company-box)
-
-(package! js2-mode)
-(package! js2-refactor)
-;(package! xref-js2)
-(package! company-tern)
-(package! ac-js2)
-(package! web-beautify)
-;(package! auto-complete)
-;(package! tern-auto-complete)
-(package! tern)
-(package! react-snippets)
-;(package! smart-tabs-mode)
-(package! web-mode)
+(package! treemacs)
+(package! treemacs-evil)
+(package! treemacs-magit)
+(package! treemacs-icons-dired)
+(package! treemacs-projectile)
+;; (Package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
