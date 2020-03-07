1018e42464c323ca69bef8b914056d9e3abd2034
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-03-07 23:41:14 +0330

    just some update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,1 @@
-alias music_en='youtube-dl -f 140  --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c  -o "/home/geeksesi/Music/En/%(title)s.%(ext)s"'
-
-
+alias music_en='youtube-dl -f 140  --embed-thumbnail --add-metadata --xattrs  --no-playlist --mark-watched -c  -o "$HOME/Music/En/%(title)s.%(ext)s"'
