1f67aca5241e874e19a72c6a12adbd917e99d8fc
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-04-28 16:22:32 +0430

    wip i want to change OS

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+alias AVPN 'sudo openconnect  --reconnect-timeout 20 --authgroup=cisco -dbu geeksesi sv30.fzserver.com:4443'
+alias BVPN 'sudo -b openvpn --config Downloads/firefox/sv103-us-combined.ovpn'
+set PATH $PATH /home/geeksesi/.nvm/versions/node/v15.4.0/bin/ 
+
